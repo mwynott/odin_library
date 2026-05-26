b661189962718e028aed1f21a2225d9802126fc5
mwynott <mwynott1989@gmail.com>
2026-05-26 18:33:32 -0400

    Add boilerplate for script, index, and style sheet

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+
+function Book(title, author, pages, isread,) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isread = isread;
+}
+
+function addBookToLibrary() {
+}
