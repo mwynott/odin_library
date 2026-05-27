277cc0a3dd9f105d3100ea4c7b529d8f7e3f5025
mwynott <mwynott1989@gmail.com>
2026-05-26 20:21:44 -0400

    Add event listeners for new book/reset form/submit form buttons

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Initial library with 6 books */
 const myLibrary = [
     new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, true),
     new Book("To Kill a Mockingbird", "Harper Lee", 281, false),
@@ -7,7 +8,7 @@ const myLibrary = [
     new Book("The Hobbit", "J.R.R. Tolkien", 310, false)
 ];
 
-
+/* Book constructor function */
 function Book(title, author, pages, isread) {
     this.title = title;
     this.author = author;
@@ -16,18 +17,55 @@ function Book(title, author, pages, isread) {
     this.id = crypto.randomUUID();
 }
 
+/* Function to place new book to the library */
 function addBookToLibrary() {
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const pages = parseInt(document.getElementById("pages").value, 10);
-    const isread = document.getElementById("isread").checked;
+    const isread = document.getElementById("isread").checked
 
     const newBook = new Book(title, author, pages, isread);
     myLibrary.push(newBook);
     displayLibrary();
 }
 
+
+const addBookForm = document.getElementById("add-book-form");
+const addBookBtn = document.getElementById("add-book-btn");
+const bookFormContainer = document.getElementById("book-form-container");
+const cancelBtn = document.getElementById("cancel-btn");
+
+/* Event listeners for buttons and form submission */
+addBookBtn.addEventListener("click", () => {
+    if (bookFormContainer) {
+        bookFormContainer.style.display = "block";
+    }
+});
+
+/* Event listener for cancel button to hide the form and reset it */
+cancelBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (bookFormContainer) {
+        bookFormContainer.style.display = "none";
+    }
+    addBookForm.reset();
+});
+
+/* Event listener for form submission to add a new book and reset the form */
+addBookForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addBookToLibrary();
+    addBookForm.reset();
+    if (bookFormContainer) {
+        bookFormContainer.style.display = "none";
+    }
+})
+
+
+/* Function to display content of the library in the table */
 function displayLibrary() {
+    const tableBody = document.getElementById("book-list-body");
+    tableBody.innerHTML = "";
     myLibrary.forEach(book => {
         const row = document.createElement("tr");
         row.innerHTML = `
@@ -36,8 +74,6 @@ function displayLibrary() {
             <td>${book.pages}</td>
             <td>${book.isread ? "Yes" : "No"}</td>
             <td><button onclick="removeBook('${book.id}')">Remove</button></td>`;
-        document.getElementById("libraryTable").appendChild(row);
+        tableBody.appendChild(row);
     });
 }
-
-displayLibrary();
